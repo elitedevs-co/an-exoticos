de38e58003cd6bdd8d48d1384ca7461c950d7801
Leonardo Cabeza <cabeza.leo@gmail.com>
2025-02-06 19:11:36 -0500

    fix: SEO & landing

--- src/config/footer.js
+++ src/config/footer.js
@@ -6,7 +6,7 @@ export const footerSocials = [
   },
   {
     name: 'Whatsapp',
-    url: 'https://l.instagram.com/?u=http%3A%2F%2Fwa.me%2F573003903899%2F%3Ffbclid%3DPAZXh0bgNhZW0CMTEAAaYrqcLf3Q4VC89-YxOq855fVezb_XstqFzq7cJxrli-WoCDDfAYQyewilU_aem_m6ATjw8MnPLZeaoN_ySmfw&e=AT1jAawRLZSvme6nz1olaDOSz3NU6RqBYoSHyw-GfGYObkthur5D2nNAddIFtaJ-8odFuLvAY3JjR2nb8xmBiiODFuUKupjOZg4x7eiexmu01Hos6rmSdqU',
+    url: 'https://wa.me/573003903899',
     icon: "mdi:whatsapp",
   },
 ]
